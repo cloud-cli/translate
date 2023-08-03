58f3ba8f2f8bd8396dd2269f09eb884b60b2ea8b
Darlan Alves <me@darlanalv.es>
2023-08-03 10:26:18 +0200

    fix: skip translation if no text present

--- App.vue
+++ App.vue
@@ -1,7 +1,13 @@
 <template>
-  <form @submit.prevent="onSubmit()" class="flex flex-col">
-    <textarea v-bind="source" class="p-2 bg-transparent my-4 h-20"></textarea>
-    <div class="p-2 bg-gray-200 my-4"></div>
+  <form
+    @submit.prevent="onSubmit()"
+    class="flex flex-col container mx-auto p-4"
+  >
+    <textarea
+      class="p-2 bg-transparent font-bold text-xl my-4 h-20"
+      v-bind="source"
+    ></textarea>
+    <div class="p-2 bg-gray-200 my-4 text-xl"></div>
     <div class="text-center">
       <button
         type="submit"
@@ -18,10 +24,20 @@
 <script setup>
 import { ref } from "vue";
 import translateText from "https://aifn.run/fn/228e48d5-7188-4529-bfc6-8430b4ecf8a0.js";
+
 const source = ref("");
+const busy = ref(false);
 const translation = ref("");
 
 async function onSubmit() {
-  translation.value = await translateText({ text: source.value });
+  if (!source.value) return;
+
+  busy.value = true;
+
+  try {
+    translation.value = await translateText({ text: source.value });
+  } finally {
+    busy.value = false;
+  }
 }
 </script>
