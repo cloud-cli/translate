460f76f75e5d2026e514eadd8191567d4681726c
Darlan Alves <me@darlanalv.es>
2023-08-05 14:03:51 +0200

    chore: add index

--- App.vue
+++ App.vue
@@ -5,16 +5,10 @@
   >
     <textarea
       @keyup="onKeyUp($event)"
-      class="p-2 bg-transparent h-min font-medium text-xl rounded-md border border-gray-300 text-gray-800"
+      class="p-2 bg-white h-min font-medium text-xl rounded-md text-gray-800"
       placeholder="Text..."
       v-model="source"
     ></textarea>
-    <div
-      class="p-2 h-min bg-gray-100 rounded-md border border-gray-300 text-primary font-bold text-xl overflow-y-auto"
-    >
-      {{ translation }}
-    </div>
-
     <button
       type="submit"
       :disabled="busy"
@@ -24,11 +18,16 @@
       <span v-else class="material-icons">translate</span>
       <span>Translate</span>
     </button>
+    <div
+      class="p-2 h-min bg-gray-100 rounded-md text-primary font-bold text-xl overflow-y-auto"
+    >
+      {{ translation }}
+    </div>
   </form>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, watch, onMounted } from "vue";
 import translateText from "https://aifn.run/fn/228e48d5-7188-4529-bfc6-8430b4ecf8a0.js";
 
 const source = ref("");
@@ -46,6 +45,19 @@ async function onSubmit() {
     busy.value = false;
   }
 }
+
+function debounce(fn, delay = 500) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  };
+}
+
+
+watch(source, debounce(onSubmit))
+
 
 function onKeyUp(event: KeyboardEvent) {
   if (event.code === "Enter" && event.ctrlKey) {
