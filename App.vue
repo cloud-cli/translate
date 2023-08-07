c3ce0d9e8e95cdc6e3d844b9c4d804d1fbbd31d4
Darlan Alves <me@darlanalv.es>
2023-08-07 11:03:10 +0200

    fix: change layout and ui feedback

--- App.vue
+++ App.vue
@@ -1,29 +1,35 @@
 <template>
-  <form
-    class="w-screen h-screen container mx-auto p-4 flex flex-col gap-4"
+  <div
+    class="w-screen h-screen container mx-auto p-4 flex flex-col gap-4 overflow-y-auto"
     @submit.prevent="onSubmit()"
   >
-    <textarea
-      @keyup="onKeyUp($event)"
-      class="p-2 bg-white h-min font-medium text-xl rounded-md text-gray-800"
-      placeholder="Text..."
-      v-model="source"
-    ></textarea>
-    <button
-      type="submit"
-      :disabled="busy"
-      class="bg-primary text-white px-4 py-3 rounded flex items-center mx-auto"
-    >
-      <span v-if="busy" class="material-icons animate-spin">refresh</span>
-      <span v-else class="material-icons">translate</span>
-      <span>Translate</span>
-    </button>
-    <div
-      class="p-2 h-min bg-gray-100 rounded-md text-primary font-bold text-xl overflow-y-auto"
-    >
-      {{ translation }}
+    <div class="shadow-md rounded bg-white p-2 relative">
+      <span
+        v-if="error"
+        class="material-icons absolute top-0 right-0 z-10 m-1 text-red-500"
+        >error</span
+      >
+      <textarea
+        @keyup="onKeyUp($event)"
+        class="h-min font-medium text-xl text-gray-800"
+        placeholder="Text..."
+        v-model="source"
+      ></textarea>
     </div>
-  </form>
+    <div
+      class="shadow-md rounded p-2 h-min bg-gray-100 text-primary font-bold text-xl relative"
+    >
+      <button
+        type="button"
+        class="text-primary p-2 flex items-center absolute z-10 top-0 right-0 m-1"
+        @click="onReset()"
+      >
+        <span v-if="busy" class="material-icons animate-spin">refresh</span>
+        <span v-else class="material-icons">clear</span>
+      </button>
+      <div>{{ translation }}</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -32,6 +38,7 @@ import translateText from "https://aifn.run/fn/228e48d5-7188-4529-bfc6-8430b4ecf
 
 const source = ref("");
 const busy = ref(false);
+const error = ref(false);
 const translation = ref("");
 
 async function onSubmit() {
@@ -41,8 +48,11 @@ async function onSubmit() {
 
   try {
     translation.value = await translateText({ text: source.value });
+  } catch {
+    error.value = true;
   } finally {
     busy.value = false;
+    error.value = false;
   }
 }
 
@@ -55,20 +65,24 @@ function debounce(fn, delay = 500) {
   };
 }
 
-
-watch(source, debounce(onSubmit))
-
-
 function onKeyUp(event: KeyboardEvent) {
   if (event.code === "Enter" && event.ctrlKey) {
     onSubmit();
   }
 }
+
+function onReset() {
+  source.value = "";
+  translation.value = "";
+  error.value = false;
+}
+
+watch(source, debounce(onSubmit));
 onMounted(() => document.querySelector("textarea")?.focus());
 </script>
 
 <style>
 .h-min {
-  min-height: 35vh
+  min-height: 45vh
 }
 </style>
